f61d6ad9c7a9adb1f7982ad2894bcd54d8f98177
CharlesKibet <charlesbiegon973@gmail.com>
2025-03-25 19:42:25 +0300

    styling
    background color change from black to white

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -102,11 +102,11 @@ export default function SignUp() {
     signIn(value, { callbackUrl: "/" });
   };
   return (
-    <div className="h-full flex bg-[#0c0b08] items-center justify-center">
-      <div className="p-4 rounded-lg shadow-lg w-5/6 max-w-md border border-gray-700 md:h-auto w-[80%] sm:w-[420px] sm:p-8 mx-auto">
+    <div className="h-full flex  items-center justify-center">
+      <div className="p-4 rounded-lg shadow-lg w-5/6 max-w-md border  md:h-auto w-[80%] sm:w-[420px] sm:p-8 mx-auto">
         <div className="text-center">
-          <h2 className="text-2xl font-bold mb-1 text-white">Gist Tracker</h2>
-          <p className="text-sm text-accent-foreground text-white">
+          <h2 className="text-2xl font-bold mb-1 ">Gist Tracker</h2>
+          <p className="text-sm text-accent-foreground ">
             Use email or service ,to create account
           </p>
         </div>
@@ -118,14 +118,11 @@ export default function SignUp() {
         )}
         <form onSubmit={onSubmit} className="space-y-4">
           <div className="flex-1">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-white mb-1"
-            >
+            <label htmlFor="email" className="block text-sm font-medium  mb-1">
               Email
             </label>
             <div className="relative">
-              <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+              <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-700" />
               <input
                 type="text"
                 disabled={pending}
@@ -133,7 +130,7 @@ export default function SignUp() {
                 id="email"
                 value={form.email}
                 onChange={handleChange}
-                className={`w-full h-12 pl-10 pr-4 py-2 border bg-[#131312] text-white border-gray-600
+                className={`w-full h-12 pl-10 pr-4 py-2 border  
                    
                 } ${
                   errors.email ? "border-red-500" : "border-gray-300"
@@ -155,13 +152,13 @@ export default function SignUp() {
             <label
               htmlFor="password"
               className={`block text-sm font-medium 
-               text-white mb-1
+                mb-1
               }`}
             >
               Password
             </label>
             <div className="relative">
-              <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+              <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-700" />
               <input
                 disabled={pending}
                 type={showPassword ? "text" : "password"}
@@ -171,7 +168,7 @@ export default function SignUp() {
                 value={form.password}
                 onChange={handleChange}
                 className={`w-full h-12 pl-10 pr-4 py-2 border 
-                bg-[#131312] text-white border-gray-600
+               
               
                 ${
                   errors.password ? "border-red-500" : "border-gray-300"
@@ -199,13 +196,13 @@ export default function SignUp() {
             <label
               htmlFor="password"
               className={`block text-sm font-medium 
-               text-white mb-1
+              mb-1
               `}
             >
               Confirm Password
             </label>
             <div className="relative">
-              <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+              <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-700" />
               <input
                 disabled={pending}
                 type={showPassword ? "text" : "password"}
@@ -214,8 +211,7 @@ export default function SignUp() {
                 placeholder="********"
                 value={form.confirmPassword}
                 onChange={handleChange}
-                className={`w-full h-12 pl-10 pr-4 py-2 border 
-                 bg-[#131312] text-white border-gray-600
+                className={`w-full h-12 pl-10 pr-4 py-2 border
                    ${
                      errors.confirmPassword
                        ? "border-red-500"
@@ -279,7 +275,7 @@ export default function SignUp() {
           </div>
 
           {/* Sign In Link */}
-          <p className="text-center text-sm mt-4 text-white">
+          <p className="text-center text-sm mt-4 text-black">
             Already have an account?{" "}
             <Link
               href="/sign-in"
