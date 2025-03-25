2aa809a6f3aca1438529d3fff2411ab03f8ec1f5
CharlesKibet <charlesbiegon973@gmail.com>
2025-03-25 16:37:14 +0300

    delete account
    Delete account logic section

---
+++ src/app/api/auth/delete/route.ts
@@ -0,0 +1,43 @@
+//Implementing the delete account logic
+import { NextResponse } from "next/server";
+import User from "@/models/users";
+import connectionToDatabase from "@/lib/mongodb";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+
+export async function DELETE() {
+    try {
+        const session = await getServerSession(authOptions);
+        //Check if the user has session
+        if (!session || !session?.user?.email) {
+            return NextResponse.json(
+                { message: "Unauthorized" },
+                { status: 401 }
+            );
+        }
+        await connectionToDatabase();
+
+        //Find and delete the user
+        const deletedUser = await User.findOneAndDelete({
+            email: session?.user?.email,
+        });
+
+        if (!deletedUser) {
+            return NextResponse.json(
+                { message: "User not found" },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json(
+            { message: "Account deleted successfully" },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error("Delete Account Error:", error);
+        return NextResponse.json(
+            { message: "Something went worong" },
+            { status: 500 }
+        );
+    }
+}
