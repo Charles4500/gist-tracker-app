dcf6d5529b4ea923d2da7c687728bad700f71417
CharlesKibet <charlesbiegon973@gmail.com>
2025-03-25 16:39:39 +0300

    update user information
    Logic for users to update their personal information

---
+++ src/app/api/update/route.ts
@@ -0,0 +1,46 @@
+//Update user information logic
+import { NextResponse } from "next/server";
+import User from "@/models/users";
+import connectionToDatabase from "@/lib/mongodb";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+
+export async function PATCH(requests: Request) {
+    try {
+        const session = await getServerSession(authOptions);
+
+        if (!session || !session.user?.email) {
+            return NextResponse.json(
+                { message: "Unauthorized" },
+                { status: 401 }
+            );
+        }
+
+        const { name } = await requests.json();
+
+        await connectionToDatabase();
+
+        const updatedUser = await User.findOneAndUpdate(
+            { email: session.user.email },
+            { name },
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return NextResponse.json(
+                { message: "User not found" },
+                { status: 404 }
+            );
+        }
+        return NextResponse.json(
+            { message: "Profile updated", user: updatedUser },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error("Update Profile Error: ", error);
+        return NextResponse.json(
+            { message: "Something went wrogn" },
+            { status: 500 }
+        );
+    }
+}
